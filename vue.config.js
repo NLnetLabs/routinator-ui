05b8c4f614eb4af913835d5d014fd8755d43ae07
Jasper den Hertog <jasper@plainspace.com>
2021-06-09 15:07:08 +0200

    use prod routinator API

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,8 @@ const version = JSON.parse(packageJson).version || 0;
 module.exports = {
   productionSourceMap: false,
   devServer: {
-    proxy: "http://routinator-dev.aws.nlnetlabs.nl:8323/"
+    // proxy: "http://routinator-dev.aws.nlnetlabs.nl:8323/"
+    proxy: "https://routinator-demo.aws.nlnetlabs.nl"
   },
   publicPath: '/ui/',
 
